4bc3d4d7af5499a900bc46c759bff37f479b5f3d
changherng <62918160+changherng@users.noreply.github.com>
2021-07-06 20:20:20 +0800

    Create activitypage.java

---
+++ docs/samples/java/activitypage.java
@@ -0,0 +1,12 @@
+package com.example.bicta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activitypage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitypage);
+    }
+}
